50346a8dd21841993c91ef6c9117a3d177ca66e0
harshit243 <149683774+harshit243@users.noreply.github.com>
2025-04-08 23:39:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById('signInBtn').addEventListener('click', function() {
+  window.location.href = 'signin.html';
+});
+
+document.getElementById('joinWorldBtn').addEventListener('click', function() {
+  window.location.href = 'join-world.html';
+});
+
+document.getElementById('createSpaceBtn').addEventListener('click', function() {
+  window.location.href = 'create-space.html';
+});
+
+document.getElementById('tryDemoBtn').addEventListener('click', function() {
+  window.location.href = 'demo.html';
+});
